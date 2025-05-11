138a00b4513b34b79f552fb3d6b4f53f57318297
Alejandra1401 <alejandra.reyes.dias17@gmail.com>
2025-05-11 16:33:19 -0500

    ahu

--- metodo.java
+++ metodo.java
@@ -6,42 +6,87 @@ public class metodo
     {
         String ingreso = "";
         objEst_Diseño o = new objEst_Diseño();
-        do {
-            ingreso = JOptionPane.showInputDialog("Ingrese la cédula del estudiante");
-            
-            if (ingreso == null) {
-                JOptionPane.showMessageDialog(null, "Debe ingresar la cédula del estudiante");
-            } else if (ingreso.trim().isEmpty()) {
-                JOptionPane.showMessageDialog(null, "La cédula no puede estar vacía");
-            }
-            
-        } while (ingreso == null || ingreso.trim().isEmpty());
+        ingreso = JOptionPane.showInputDialog("Ingrese el cédula del estudiante");
+        while (!ingreso.matches("\\d+")) 
+        {
+            JOptionPane.showMessageDialog(null, "Error: Cedula no válida. Solo se permiten números.");
+            ingreso = JOptionPane.showInputDialog("Ingrese la cedula del estudiante");
+        }
         o.setCedula(ingreso);
-        do {
-            ingreso = JOptionPane.showInputDialog("Ingrese la nombre del estudiante");
-            
-            if (ingreso == null || ingreso.trim().isEmpty())  {
-                JOptionPane.showMessageDialog(null, "Debe ingresar el nombre, no puede estar vacio");
-            } else if (!ingreso.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$")) {
-                JOptionPane.showMessageDialog(null, "El nombre no puede contener números ni letras especiales");
-                ingreso = ""; //para que el ciclo vuelva a repetirse 
-            }
-            
-        } while (ingreso == null || ingreso.trim().isEmpty());
+        ingreso = JOptionPane.showInputDialog("Ingrese el nombre del estudiante");
+        while (ingreso == null || ingreso.trim().isEmpty() || !ingreso.matches("[a-zA-Z ]+")) 
+        {
+            JOptionPane.showMessageDialog(null, "Error: Nombre no valido. Solo permite letras.");
+            ingreso = JOptionPane.showInputDialog("Ingrese el nombre del estudiante");
+        }
         o.setNombre(ingreso);
-        do {
+        ingreso = JOptionPane.showInputDialog("Ingrese el apellido del estudiante");
+        while (ingreso == null || ingreso.trim().isEmpty() || !ingreso.matches("[a-zA-Z ]+")) 
+        {
+            JOptionPane.showMessageDialog(null, "Error: Nombre no valido. Solo permite letras.");
             ingreso = JOptionPane.showInputDialog("Ingrese el apellido del estudiante");
-            
-            if (ingreso == null) {
-                JOptionPane.showMessageDialog(null, "Debe ingresar el apellido del estudiante");
-            } else if (ingreso.trim().isEmpty()) {
-                JOptionPane.showMessageDialog(null, "El apellido no puede estar vacío");
-            }
-            
-        } while (ingreso == null || ingreso.trim().isEmpty());
+        }
         o.setApellido(ingreso);
-        
+        ingreso = JOptionPane.showInputDialog("Ingrese el telefono del estudiante");
+        while (!ingreso.matches("\\d+")) 
+        {
+            JOptionPane.showMessageDialog(null, "Error: Telefono no válido. Solo se permiten números.");
+            ingreso = JOptionPane.showInputDialog("Ingrese el telefono del estudiante");
+        }
+        o.setTelefono(ingreso);
+        do {
+            ingreso = JOptionPane.showInputDialog(null, "Ingrese modalidad de estudio: presencial / virtual");
 
-
+            if (ingreso == null || ingreso.trim().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "La modalidad no puede estar vacía.");
+                continue;
+            }
+            ingreso = ingreso.trim().toLowerCase();
+            if (!ingreso.equals("virtual") && !ingreso.equals("presencial")) {
+            JOptionPane.showMessageDialog(null, "Modalidad inválida. Solo se permite: virtual o presencial.");
+            ingreso = ""; // fuerza repetir el ciclo
+            }
+        }while (ingreso.isEmpty());
+        o.setModalidadEstud(ingreso.substring(0, 1).toUpperCase() + ingreso.substring(1));//mayusc inicial
+        ingreso =  JOptionPane.showInputDialog(null, "Ingrese cantidad de asignaturas matriculas por el estudiante");
+        while (!ingreso.matches("\\d+")) 
+        {
+            JOptionPane.showMessageDialog(null, "Dato no valido, reintente");
+            ingreso =  JOptionPane.showInputDialog(null, "Ingrese número entero valido");
+        }
+        o.setCantAsignat(Integer.parseInt(ingreso));
+        return validarExisteEstDis(lisDis,o);//No vaya a ingresar estudiante exixtente
     }
+    public void mostrarEstDis (LinkedList<objEst_Diseño> listDis)
+    {
+        for (objEst_Diseño list : listDis) //Deshapilando para mostrar(es lo mismo que un for normal)
+        {
+            System.out.println("Cédula: " + list.getCedula());
+            System.out.println("Nombre: " + list.getNombre());
+            System.out.println("Apellido: " + list.getApellido());
+            System.out.println("Telefono: " + list.getTelefono());
+            System.out.println("Modalidad: " + list.getModalidadEstud());
+            System.out.println("Cant asignaturas: " + list.getCantAsignat());
+        }
+    }
+    public LinkedList<objEst_Diseño> validarExisteEstDis(LinkedList<objEst_Diseño> lisDis, objEst_Diseño ingresoEstDis )
+    {
+        boolean encontrado = false;
+        for(objEst_Diseño est : lisDis)
+        {
+            if (est.getCedula().equalsIgnoreCase(ingresoEstDis.getCedula())) 
+            {
+                encontrado = true;
+                JOptionPane.showMessageDialog( null, "El estudiante ya existe");
+                break;
+            }
+        }
+        if(!encontrado)
+        {
+            lisDis.offer(ingresoEstDis);
+            JOptionPane.showMessageDialog(null, "Estudiante ingresado correctamente");
+        }
+        mostrarEstDis(lisDis);
+        return lisDis; 
+    } 
 }
